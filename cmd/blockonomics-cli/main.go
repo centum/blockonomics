009d4b1d948436327635999cb89e3ebee4df26d5
Vadim Statishin <statishin@gmail.com>
2020-03-02 23:58:02 +0200

    refactor: main

--- cmd/blockonomics-cli/main.go
+++ cmd/blockonomics-cli/main.go
@@ -10,58 +10,57 @@ import (
 )
 
 func main() {
-	switch {
-	case len(os.Args) > 1 && os.Args[1] == "balance":
-		cmd := flag.NewFlagSet("balance", flag.ExitOnError)
-		addr := cmd.String("addr", "", "Whitespace separated list of bitcoin addresses/xpubs")
-		cmd.Parse(os.Args[2:])
-
-		api := blockonomics.NewClient("", blockonomics.WithTimeout(30*time.Second))
-		dump(api.Balance(*addr))
-
-	case len(os.Args) > 1 && os.Args[1] == "searchhistory":
-		cmd := flag.NewFlagSet("searchhistory", flag.ExitOnError)
-		addr := cmd.String("addr", "", "Whitespace separated list of bitcoin addresses/xpubs")
-		cmd.Parse(os.Args[2:])
-
-		api := blockonomics.NewClient("", blockonomics.WithTimeout(30*time.Second))
-		dump(api.SearchHistory(*addr))
-
-	case len(os.Args) > 1 && os.Args[1] == "tx_detail":
-		cmd := flag.NewFlagSet("tx_detail", flag.ExitOnError)
-		txid := cmd.String("txid", "", "transaction id")
-		cmd.Parse(os.Args[2:])
-
-		api := blockonomics.NewClient("", blockonomics.WithTimeout(30*time.Second))
-		dump(api.TxDetail(*txid))
-
-	case len(os.Args) > 1 && os.Args[1] == "addr":
-		cmd := flag.NewFlagSet("addr", flag.ExitOnError)
-		token := cmd.String("token", "", "access token to API Blockonomics")
-		account := cmd.String("account", "", "get address for account")
-		reset := cmd.Bool("reset", false, "reset prev address")
-		cmd.Parse(os.Args[2:])
-
-		api := blockonomics.NewClient(*token, blockonomics.WithTimeout(30*time.Second))
-		dump(api.NewAddress(*account, *reset))
-
-	case len(os.Args) > 1 && os.Args[1] == "invoice":
-		cmd := flag.NewFlagSet("invoice", flag.ExitOnError)
-		addr := cmd.String("addr", "", "Invoice for address")
-		amount := cmd.Float64("amount", 0, "Invoice amount")
-		currency := cmd.String("currency", "USD", "Invoice currency")
-		description := cmd.String("description", "", "Invoice description")
-		invoiceLive := cmd.Duration("live", 1*time.Hour, "Invoice live time")
-		cmd.Parse(os.Args[2:])
-
-		api := blockonomics.NewClient("", blockonomics.WithTimeout(30*time.Second))
-		dump(api.Invoice(*addr, *amount, *currency, *description, time.Now().Add(*invoiceLive)))
-
-	default:
-		fmt.Printf("%q is not valid command.\n", os.Args[1])
-		fmt.Printf("usage: %s <command> [<args>]", os.Args[0])
-		os.Exit(-1)
+	var (
+		token, addr, txid, account, currency, description string
+		amount                                            float64
+		reset                                             bool
+		invoiceLive                                       time.Duration
+	)
+	if len(os.Args) <= 1 {
+		usage()
 	}
+	cmd := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
+	switch cmd.Name() {
+	case "balance":
+		cmd.StringVar(&addr, "addr", "", "Whitespace separated list of bitcoin addresses/xpubs")
+	case "searchhistory":
+		cmd.StringVar(&addr, "addr", "", "Whitespace separated list of bitcoin addresses/xpubs")
+	case "tx_detail":
+		cmd.StringVar(&txid, "txid", "", "transaction id")
+	case "addr":
+		cmd.StringVar(&token, "token", "", "access token to API Blockonomics")
+		cmd.StringVar(&account, "account", "", "get address for account")
+		cmd.BoolVar(&reset, "reset", false, "reset prev address")
+	case "invoice":
+		cmd.StringVar(&addr, "addr", "", "Invoice for address")
+		cmd.Float64Var(&amount, "amount", 0, "Invoice amount")
+		cmd.StringVar(&currency, "currency", "USD", "Invoice currency")
+		cmd.StringVar(&description, "description", "", "Invoice description")
+		cmd.DurationVar(&invoiceLive, "live", 1*time.Hour, "Invoice live time")
+	default:
+		usage()
+	}
+	cmd.Parse(os.Args[2:])
+
+	api := blockonomics.NewClient(token, blockonomics.WithTimeout(30*time.Second))
+
+	switch cmd.Name() {
+	case "balance":
+		dump(api.Balance(addr))
+
+	case "searchhistory":
+		dump(api.SearchHistory(addr))
+
+	case "tx_detail":
+		dump(api.TxDetail(txid))
+
+	case "addr":
+		dump(api.NewAddress(account, reset))
+
+	case "invoice":
+		dump(api.Invoice(addr, amount, currency, description, time.Now().Add(invoiceLive)))
+	}
+
 }
 
 func dump(v interface{}, err error) {
@@ -73,4 +72,10 @@ func dump(v interface{}, err error) {
 		fmt.Println(err.Error())
 	}
 	fmt.Println(string(d))
+}
+
+func usage() {
+	fmt.Printf("%q is not valid command.\n", os.Args[1])
+	fmt.Printf("usage: %s <command> [<args>]", os.Args[0])
+	os.Exit(-1)
 }
